c1eabb42d5b70a54b365bc92f192af13a57b6bb1
Ben Jesty <benjamin.jesty@cancer.org.uk>
2026-01-28 20:40:36 +0000

    started acrostics

---
+++ src/projects/general/acrostics.py
@@ -0,0 +1,8 @@
+from data_index.raw import NARROW_WORD_LIST
+from data_index.opted import OPTED_VALID
+
+
+input_words = [["access", "inside", "go", "open"], ["room", "enter"], ["unlock", "step"]]
+
+for word in OPTED_VALID.load():
+    if len()
